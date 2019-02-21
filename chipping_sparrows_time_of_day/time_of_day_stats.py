dcb5d4f1e388d816a0cd41e1bbc7cb37cc904d03
asearfos <abigail.m.searfoss@gmail.com>
2019-02-20 22:05:37 -0600

    Updated the input files and data combining to work with TOD csv files. Removed civil twilight from rank-sums tests as I just wanted to use sunrise. Did not want to over test and also the sample sizes were very small if using any twilights.

--- chipping_sparrows_time_of_day/time_of_day_stats.py
+++ chipping_sparrows_time_of_day/time_of_day_stats.py
@@ -15,17 +15,21 @@ from matplotlib.ticker import FuncFormatter
 Load data and organize/subset wilcoxon rank sums test 
 """
 # load in song data
-data_path = 'C:/Users/abiga\Box Sync\Abigail_Nicole\ChippiesProject\FinalDataCompilation' + \
-            '/FinalDataframe_CombinedTables_withReChipper_thenWithReExportedAs44100Hz_LogTransformed.csv'
+data_path = "C:/Users/abiga/Box " \
+            "Sync/Abigail_Nicole/ChippiesTimeOfDay" \
+            "/FinalChippiesDataReExportedAs44100Hz_LogTransformed_forTOD.csv"
 log_song_data = pd.DataFrame.from_csv(data_path, header=0, index_col=None)
 
-col_to_skip = ['FromDatabase', 'Latitude', 'Longitude', 'RecordingDay', 'RecordingMonth', 'RecordingYear',
-               'RecordingTime']
+col_to_skip = ['Latitude', 'Longitude', 'RecordingDay',
+               'RecordingMonth', 'RecordingYear', 'RecordingTime',
+               'RecordingTimeSeconds']
 data_subset = log_song_data.drop(col_to_skip, axis=1)
 
 # load in time data --> before or after sunrise, twilights, and noon (only going to use sunrise and noon)
-data_path = "C:/Users/abiga\Box Sync\Abigail_Nicole\ChippiesProject\StatsOfFinalData_withReChipperReExported\TimeAnalysis" \
-            "\FinalData_LogTransformed_relativeToSunriseTwilightsNoon.csv"
+data_path = "C:/Users/abiga/Box " \
+            "Sync/Abigail_Nicole/ChippiesTimeOfDay" \
+            "/FinalChippiesDataReExportedAs44100Hz_LogTransformed" \
+            "_forTOD_SunriseTwilightNoon.csv"   
 time_data = pd.DataFrame.from_csv(data_path, header=0, index_col=None)
 # must remove duplicates -- have more than one bird from same recording -- duplicate catalog number and time data
 time_data = time_data.drop_duplicates()
@@ -33,47 +37,47 @@ time_data = time_data.drop_duplicates()
 # combine tables using catalog no
 combined_df = pd.merge(data_subset, time_data, on='CatalogNo')
 
-# only keep unique and use and ones with time data
-combined_df_unique = combined_df.loc[combined_df['ComparedStatus'].isin(['unique', 'use'])].copy().reset_index(
+# only keep ones with time data
+combined_df = combined_df.drop(combined_df[combined_df.Sunrise == 
+                                           '--'].index).copy().reset_index(
     drop=True)
-combined_df_unique = combined_df_unique.drop(combined_df_unique[combined_df_unique.Sunrise ==
-                                                                '--'].index).copy().reset_index(drop=True)
+
+print(combined_df.columns)
+print(combined_df.groupby('Sunrise').count()['CatalogNo'])
 
 """
 Wilcoxon Ranksums
 """
 
-with open('C:/Users/abiga/Box Sync/Abigail_Nicole/ChippiesProject/StatsOfFinalData_withReChipperReExported/TimeAnalysis'
-          '/SunriseCivilTwilightNoon_WilcoxonRanksums.csv', 'wb') as file:
+with open("C:/Users/abiga/Box "
+          "Sync/Abigail_Nicole/ChippiesTimeOfDay/TODDiscrete"
+          "/SunriseCivilTwilightNoon_WilcoxonRanksums.csv",
+          'wb') as file:
     filewriter = csv.writer(file, delimiter=',')
     filewriter.writerow(['Song Variable',
                          'Before After Sunrise p-value',
                          'Before Sunrise and After Noon p-value',
-                         'After Sunrise and After Noon p-value',
-                         'Before After Civil Twilight p-value',
-                         'Before Civil Twilight and After Noon p-value',
-                         'After Civil Twilight and After Noon p-value'])
+                         'After Sunrise and After Noon p-value'
+                         ])
 
-    song_variables = ['Duration of Song Bout (s)',
-                      'Total Number of Syllables (number)']
+    song_variables = ['Duration of Song Bout (log(ms))',
+                      'Mean Syllable Duration (log(ms))',
+                      'Mean Inter-Syllable Silence Duration (log(ms))',
+                      'Total Number of Syllables (log(number))']
 
-    song_units = ['log(ms)', 'log(number)']
-
-    for sv in ['BoutDuration_ms', 'NumSyllables']:
-        before_sunrise = combined_df_unique.loc[combined_df_unique['Sunrise'] == 'before sunrise', sv]
-        after_sunrise = combined_df_unique.loc[combined_df_unique['Sunrise'] == 'after sunrise', sv]
-        sunrise_after_noon = combined_df_unique.loc[combined_df_unique['Sunrise'] == 'after noon', sv]
-
-        before_civil = combined_df_unique.loc[combined_df_unique['CivilTwilight'] == 'before civil', sv]
-        after_civil = combined_df_unique.loc[combined_df_unique['CivilTwilight'] == 'after civil', sv]
-        civil_after_noon = combined_df_unique.loc[combined_df_unique['CivilTwilight'] == 'after noon', sv]
+    for sv in song_variables:
+        before_sunrise = combined_df.loc[combined_df['Sunrise'] ==
+                                         'before sunrise', sv]
+        after_sunrise = combined_df.loc[combined_df['Sunrise'] ==
+                                        'after sunrise', sv]
+        sunrise_after_noon = combined_df.loc[combined_df['Sunrise'] ==
+                                             'after noon', sv]
 
         filewriter.writerow([sv, ranksums(before_sunrise, after_sunrise)[1],
                              ranksums(before_sunrise, sunrise_after_noon)[1],
-                             ranksums(after_sunrise, sunrise_after_noon)[1],
-                             ranksums(before_civil, after_civil)[1],
-                             ranksums(before_civil, civil_after_noon)[1],
-                             ranksums(after_civil, civil_after_noon)[1]])
+                             ranksums(after_sunrise, sunrise_after_noon)[1]
+                             ])
+quit()
 """"
 Box plots (change out the sv, the index for title, and the formatting for the two different box plot sets - bout 
 duration and number of syllables)
@@ -83,9 +87,9 @@ duration and number of syllables)
 for sv in ['NumSyllables']:  # for num of sylls
     fig = plt.figure(figsize=(7, 11))
     sns.set(style='white')
-    ax = sns.boxplot(x='CivilTwilight', y=sv, data=combined_df_unique[['CivilTwilight', sv]], color='None',
+    ax = sns.boxplot(x='CivilTwilight', y=sv, data=combined_df[['CivilTwilight', sv]], color='None',
                      fliersize=0, width=0.5, linewidth=2, order=['before civil', 'after civil', 'after noon'])
-    ax = sns.stripplot(x='CivilTwilight', y=sv, data=combined_df_unique[['CivilTwilight', sv]],
+    ax = sns.stripplot(x='CivilTwilight', y=sv, data=combined_df[['CivilTwilight', sv]],
                        order=['before civil', 'after civil', 'after noon'],
                        palette=['black', '#95B2B8', '#F1D302'], size=7, jitter=True, lw=1, alpha=0.6)
 
@@ -116,9 +120,9 @@ for sv in ['NumSyllables']:  # for num of sylls
 
     fig = plt.figure(figsize=(7, 11))
     sns.set(style='white')
-    ax = sns.boxplot(x='Sunrise', y=sv, data=combined_df_unique[['Sunrise', sv]], color='None',
+    ax = sns.boxplot(x='Sunrise', y=sv, data=combined_df[['Sunrise', sv]], color='None',
                      fliersize=0, width=0.5, linewidth=2, order=['before sunrise', 'after sunrise', 'after noon'])
-    ax = sns.stripplot(x='Sunrise', y=sv, data=combined_df_unique[['Sunrise', sv]],
+    ax = sns.stripplot(x='Sunrise', y=sv, data=combined_df[['Sunrise', sv]],
                        order=['before sunrise', 'after sunrise', 'after noon'],
                        palette=['black', '#95B2B8', '#F1D302'], size=7, jitter=True, lw=1, alpha=0.6)
 
