09f4d81dda37feab5e476bba4bafe5dbb8adc3df
asearfos <asearfos@gmail.com>
2019-07-24 09:51:18 -0500

    Removed plot of locations and year. The year was irrelevant and the distribution of all the data is now done in ArcGIS.

--- chipping_sparrows_time_of_day/geographic_spread_of_data.py
+++ chipping_sparrows_time_of_day/geographic_spread_of_data.py
@@ -13,7 +13,7 @@ from mpl_toolkits.basemap import Basemap
 
 
 """
-Load data and organize/subset wilcoxon rank sums test 
+Load data and organize/subset 
 """
 # load in song data
 data_path = "C:/Users/abiga/Box " \
@@ -34,59 +34,22 @@ time_data = time_data.drop_duplicates()
 
 # combine tables using catalog no
 combined_df = pd.merge(log_song_data, time_data, on='CatalogNo')
+print(combined_df.shape)
+# combined_df.to_csv("C:/Users/abiga\Box "
+#                    "Sync\Abigail_Nicole\ChippiesTimeOfDay\RecordingInfo"
+#                    "/TimeOfDayPaper_RecordingsUsed_wData.csv", index=False)
 
 # only keep ones with time data
 combined_df = combined_df.drop(combined_df[combined_df.Sunrise ==
                                            '--'].index).copy().reset_index(
     drop=True)
 
-print(combined_df.shape)
-
-""""
-geographical distribution of data by year
-"""
-
-# plot locations of all the song data collected --> this includes for all
-# regions the unique songs and all songs
-# chosen as use for possible duplicates
-
-my_dpi = 96
-fig = plt.figure(figsize=(2600 / my_dpi, 1800 / my_dpi), dpi=my_dpi,
-                 frameon=False)
-
-# make the background map
-# m = Basemap(llcrnrlat=8, llcrnrlon=-169, urcrnrlat=72, urcrnrlon=-52)
-m = Basemap(llcrnrlat=8, llcrnrlon=-169, urcrnrlat=72, urcrnrlon=-52, projection='merc')
-m.drawcoastlines(color='k', linewidth=1.5)
-m.drawcountries(color='k', linewidth=1.5)
-m.drawstates(color='gray')
-m.drawmapboundary(fill_color='w', color='none')
-
-xpts, ypts = m(np.asarray(combined_df['Longitude']), np.asarray(combined_df['Latitude']))
-m.scatter(xpts, ypts, marker='o',
-          c=combined_df['RecordingYear'], cmap='cool', edgecolors='k',
-          linewidth=0.5, s=200)
-cb = m.colorbar()
-field_x, field_y = m(-72.5170, 42.3709)
-m.scatter(field_x, field_y, marker='*', color='gold', zorder=5,
-          edgecolor='k', s=500)
-
-cb.ax.tick_params(labelsize=25)
-
-plt.tight_layout()
-
-pdf = PdfPages("C:/Users/abiga/Box "
-               "Sync/Abigail_Nicole/ChippiesTimeOfDay/GeographicSpread_Year"
-               ".pdf")
-
-pdf.savefig(dpi=fig.dpi, orientation='landscape', transparent=True)
-pdf.close()
-
-plt.show()
 
 """"
 Geographic distribution of song duration for dawn
 """
+
+# supplemental figure
 
 dawn_only = combined_df[combined_df['Sunrise'] == 'before sunrise']
 print(dawn_only.shape)
